2dd4485af5578f464a63861a0389aaffc0037cab
Issei Kuzumaki <mail@isseikuzumaki.tokyo>
2022-08-09 17:00:31 +0900

    0.0.2

--- README.md
+++ README.md
@@ -1,21 +1,11 @@
-# Hello world javascript action
+# Send Pull Requests
 
 This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
 
 ## Inputs
 
-## `who-to-greet`
+## `github-token`
 
-**Required** The name of the person to greet. デフォルトは `"World"`。
+**Required** 
 
-## Outputs
-
-## `time`
-
-The time we greeted you.
-
-## 使用例
-
-uses: actions/hello-world-javascript-action@v1.1
-with:
-  who-to-greet: 'Mona the Octocat'
+## Example
--- index.js
+++ index.js
@@ -2,14 +2,31 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  main();
 } catch (error) {
   core.setFailed(error.message);
+}
+
+function main() {
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+
+  const gitHubToken = core.getInput('github-token').trim();
+  const octokit = github.getOctokit(githubToken);
+
+  const diff = getDiffFromMain();
+  const branch = getBranchName();
+  postDiffToServer(diff, branch);
+}
+
+async function getDiffFromMain() {
+
+}
+
+async function getBranchName() {
+
+}
+
+async function postDiffToServer(branch, diff) {
+
 }
