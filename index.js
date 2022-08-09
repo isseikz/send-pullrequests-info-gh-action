bb10ca3825fe51fbc3c2419b2c651365939c84c2
isseikz <issey.macky@gmail.com>
2022-08-10 02:12:02 +0900

    Add credential

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const github = require('@actions/github');
 const util = require('node:util');
 const exec = util.promisify(require("child_process").exec);
 
+
+const { initializeApp } = require('firebase/app');
+const { getFirestore, collection } = require('firebase/firestore/lite');
+
 try {
   main();
 } catch (error) {
@@ -31,4 +35,24 @@ async function getDiffFromMain() {
 async function postDiffToServer(diff, branch) {
   console.log(`branch: ${branch}`);
   console.log(`diff: ${diff}`);
+
+  const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+  };
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  const commitsCol = collection(db, 'simplified-commit');
+  const data = {
+    patch: diff,
+    sha: "a"
+  };
+
+
+  const res = await commitsCol.doc().set(data);
+  return commits;
 }
